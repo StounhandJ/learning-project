057f3d78168726a4da7fef254f66cde1479893f5
StounhandJ <rmarinicev@gmail.com>
2021-01-21 19:07:58 +0300

    first

---
+++ Cpp/Pro/cmake-build-debug/CMakeFiles/Pro.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pro.dir/main.cpp.obj"
+  "Pro.exe"
+  "Pro.exe.manifest"
+  "Pro.lib"
+  "Pro.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pro.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Cpp/Pro/cmake-build-debug/CMakeFiles/Pro.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Pro.
+# This may be replaced when dependencies are built.
