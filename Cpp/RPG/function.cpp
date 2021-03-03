6e3164b34395758d5707a09cebe52e1447c6ccc1
StounhandJ <rmarinicev@gmail.com>
2021-03-04 01:41:01 +0300

    clear

---
+++ Cpp/RPG/function.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rmari on 04.03.2021.
+//
+
+#include "function.h"
---
+++ Cpp/RPG/function.h
@@ -0,0 +1,14 @@
+//
+// Created by rmari on 04.03.2021.
+//
+
+#ifndef RPG_FUNCTION_H
+#define RPG_FUNCTION_H
+
+
+class function {
+
+};
+
+
+#endif //RPG_FUNCTION_H
